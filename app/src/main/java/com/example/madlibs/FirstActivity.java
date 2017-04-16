75d2c56a661eb0c5cc72216e5971ac18bc75f20a
HN-Le <hy.nhu.le@gmail.com>
2017-04-16 21:02:10 +0200

    Reading Strings works (no Story yet)

--- app/src/main/java/com/example/madlibs/FirstActivity.java
+++ app/src/main/java/com/example/madlibs/FirstActivity.java
@@ -1,25 +1,65 @@
 package com.example.madlibs;
 
-import android.content.Intent;
+import android.content.Context;
+
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
+import static com.example.madlibs.R.id.word_counter;
 
 
 public class FirstActivity extends AppCompatActivity {
+    TextView word_counter;
+    private Context context = this;
+    private TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-    }
+        textView = (TextView) findViewById(R.id.word_counter);
 
-    public void use_clicked(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-    }
+        AssetManager assetManager = context.getAssets();
 
+        try
+        {
+            InputStream inputStream = assetManager.open("madlib0_simple.txt");
+            Scanner scanner = new Scanner(inputStream);
+            String output = "";
+
+            while (scanner.hasNextLine()) {
+                output += scanner.nextLine() + '\n';
+            }
+
+            Story text_new = new Story(inputStream);
+            text_new.getPlaceholderCount();
+
+            textView.setText(output);
+        }
+
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
 }
+
--- app/src/main/java/com/example/madlibs/SecondActivity.java
+++ app/src/main/java/com/example/madlibs/SecondActivity.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.io.InputStreamReader;
 
 import static android.R.attr.tag;
 import static android.R.id.message;
+import static com.example.madlibs.R.id.word_counter;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -22,21 +24,22 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
     }
 
-////    loading text files in
+    //        AssetManager am = context.getAssets();
 //
-//    public void read (Context) throws IOException {
-//
-//        AssetManager am = context.getAssets();
-//        InputStream inputStream = openFileInput("madlib0_simple.txt");
+//        InputStream inputStream = openFileInput("madlib1_tarzan.txt");
 //        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 //        BufferedReader bufferdReader = new BufferedReader(inputStreamReader);
 //
+//        Story text_new = new Story(inputStream);
+//        text_new.getPlaceholderCount();
+//        word_counter  = (TextView)findViewById(R.id.word_counter);
+//        word_counter.setText((CharSequence) text_new);
+
 //        StringBuilder total = new StringBuilder();
 //        String text;
 //        while ((text = bufferdReader.readLine()) != null) {
 //            total.append(text).append('\n');
 //        }
-//    }
-//
-//    private Context context = this;
+//        private Context context = this;
+
 }
